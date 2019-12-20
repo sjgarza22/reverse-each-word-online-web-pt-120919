eac44de709aac85d0fdf4c3f3a00dee64dbe81ef
Sara Garza <sjgarza22@gmail.com>
2019-12-20 04:54:07 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,18 @@
+#def reverse_each_word(string)
+#  array = string.split
+#  array.each_with_index do |word, index|
+#    array[index] = word.reverse
+#  end
+#  string = array.join(" ")
+#  puts string
+#  string
+#end
+
+def reverse_each_word(string)
+  array = string.split
+  array = array.collect do |word|
+    word.reverse
+  end
+  string = array.join(" ")
+  string
+end
